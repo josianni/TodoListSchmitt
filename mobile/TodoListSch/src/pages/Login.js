86fcc7686daa9e1a9b534446fd37704790ea0bcd
Renan Schmitt <renanschmitt@gmail.com>
2020-09-07 22:11:00 -0300

    fix warning

--- mobile/TodoListSch/src/pages/Login.js
+++ mobile/TodoListSch/src/pages/Login.js
@@ -40,7 +40,7 @@ export default function Login({ navigation }) {
             <TextInput
                 style={styles.input}
                 autoCapitalize='none'
-                autoCorrect='false'
+                autoCorrect={false}
                 placeholder='Digite seu Nome'
                 placeholderTextColor='#999'
                 value={user}
@@ -49,7 +49,7 @@ export default function Login({ navigation }) {
             <TextInput
                 style={styles.input}
                 autoCapitalize='none'
-                autoCorrect='false'
+                autoCorrect={false}
                 placeholder='Digite seu e-mail'
                 placeholderTextColor='#999'
                 value={email}
